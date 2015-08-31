d2f1819b4284fe212e77b6ef7945fdcfebaacbab
Joe Fiorini <joe@joefiorini.com>
2015-08-31 16:01:04 -0400

    Add wahoo theme

---
+++ .wahoo/themes/joesimple/fish_greeting.fish
@@ -0,0 +1,12 @@
+# Set global color styles, for example:
+#
+# function joesimple_error
+#   set_color -o red
+# end
+#
+# function joesimple_normal
+#   set_color normal
+#
+
+function fish_greeting
+end
