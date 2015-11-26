f8e995ed6d8459ac2b1434be052c8ef45a0eb55e
Joe Fiorini <joe@joefiorini.com>
2015-11-26 17:01:17 -0500

    Add function to dynamically add ./bin to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,19 @@ set -g WAHOO_PATH $HOME/.wahoo
 set -g WAHOO_CUSTOM $HOME/.dotfiles
 source $WAHOO_PATH/init.fish
 
-function ll --description 'List contents of directory using long format'
-	ls -lahv $argv
-end
+set EXTRA_PATHS ./bin ./node_modules/.bin
 
-set PATH ./node_modules/.bin $PATH
-# set PATH ./.cabal-sandbox/bin $PATH
-# set PATH $HOME/.cabal/bin $PATH
-set PATH $HOME/bin $PATH
+function __check_bin --on-variable PWD --description 'Add ./bin to PATH if it exists in CWD'
+  status --is-command-substitution; and return
+
+  for check_path in $EXTRA_PATHS
+    if contains $check_path $PATH
+      set item_index (contains --index $check_path $PATH)
+      set -e PATH[$item_index]
+    end
+
+    if test -d $check_path
+      set -x PATH $check_path $PATH
+    end
+  end
+end
