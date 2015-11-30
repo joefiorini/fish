5b65a33b4017673b9593f4c3b9f1dd87abc5e1e8
Joe Fiorini <joe@joefiorini.com>
2015-11-30 10:26:45 -0500

    Add /Volumes/Shared/Users/joe/bin to /Volumes/Shared/Users/joe/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ set -g WAHOO_PATH $HOME/.wahoo
 set -g WAHOO_CUSTOM $HOME/.dotfiles
 source $WAHOO_PATH/init.fish
 
-set EXTRA_PATHS ./bin ./node_modules/.bin
+set EXTRA_PATHS ~/bin ./bin ./node_modules/.bin
 
 function __check_bin --on-variable PWD --description 'Add ./bin to PATH if it exists in CWD'
   status --is-command-substitution; and return
